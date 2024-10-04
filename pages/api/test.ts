f8ebf10f9bdde58f270ee5441633227d3b55bb8a
kilogold <bonilla_kelvin@hotmail.com>
2024-10-04 17:17:44 -0400

    Cleanup. Removal of all things Anchor. Removed stale route pages.

--- pages/api/test.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log('Test route called');
-  res.status(200).json({ message: 'Test route is working' });
-}
